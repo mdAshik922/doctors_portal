dec09431a50349cc889b48a6cbd10054f70e82f0
mdAshik922 <md.ashik0005723@gmail.com>
2022-01-30 21:59:39 +0600

    commit

--- src/Pages/Appointment/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment/Appointment.js
@@ -4,7 +4,7 @@ import AppointmentHeader from '../AppointmentHeader/AppointmentHeader';
 import AvailableAppointments from '../AvailableAppointments/AvailableAppointments';
 
 const Appointment = () => {
-    const [date, setDate] = React.useState(new Date());
+    const [ date, setDate ] = React.useState(new Date());
     return (
         <div>
             <Navigation></Navigation>
